de1307e4930aaf30ac40225884b1740b081fdf41
Noah Ratcliff <me@nratcliff.com>
2015-10-10 19:55:15 -0400

    Nevermind, packing algorithms are hard

--- SPOOPY/Assets/Resources/Scripts/Pairing.cs
+++ SPOOPY/Assets/Resources/Scripts/Pairing.cs
@@ -2,67 +2,86 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//public enum FurnatureType
+//{
+//    Desk,
+//    Bed,
+//    BunkBed,
+//    FilingCabinet
+//}
+
 public class Pairing : MonoBehaviour
 {
-    public bool StrongObject; //check parings first
-    public List<GameObject> StrongPairs; //first round pairings
-    public List<GameObject> WeakPairs; //second round pairings
-    public List<GameObject> CurrentPairs; //pairs made by program
-    private GameObject room;
-    // Use this for initialization
-    void Start()
-    {
-        GameObject room = GameObject.FindGameObjectWithTag("Room");
-        if (!room)
-            Debug.LogWarning(this.name + " - Failed to locate Room!");
+    //public static Dictionary<KeyValuePair<FurnatureType, FurnatureType>, int> PairingStrengths = new Dictionary<KeyValuePair<FurnatureType, FurnatureType>, int>()
+    //{
+    //    {new KeyValuePair<FurnatureType, FurnatureType>(FurnatureType.Desk, FurnatureType.FilingCabinet), 5},
+    //    {new KeyValuePair<FurnatureType, FurnatureType>(FurnatureType.Desk, FurnatureType.Bed), 2},
+    //    {new KeyValuePair<FurnatureType, FurnatureType>(FurnatureType.FilingCabinet, FurnatureType.Bed), 2},
+    //    {new KeyValuePair<FurnatureType, FurnatureType>(FurnatureType.Desk, FurnatureType.BunkBed), 1},
+    //    {new KeyValuePair<FurnatureType, FurnatureType>(FurnatureType.Bed, FurnatureType.Bed), 1},
+    //};
 
-    }
+    //public FurnatureType FurnatureType;
+    //public bool StrongObject; //check parings first
+    //public List<GameObject> StrongPairs; //first round pairings
+    //public List<GameObject> WeakPairs; //second round pairings
+    //public List<GameObject> CurrentPairs; //pairs made by program
+    //private GameObject room;
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    GameObject room = GameObject.FindGameObjectWithTag("Room");
+    //    if (!room)
+    //        Debug.LogWarning(this.name + " - Failed to locate Room!");
 
-    // Update is called once per frame
-    void Update()
-    {
+    //}
 
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
 
     //public int GetStrength(GameObject other)
     //{
-        
+    //    return PairingStrengths.
     //}
 
-    public List<GameObject> GetStrongPairsInScene()
-    {
-        GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("RoomObject");
-        return getPairs(StrongPairs, roomObjects);
-    }
+    //public List<GameObject> GetStrongPairsInScene()
+    //{
+    //    GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("RoomObject");
+    //    return getPairs(StrongPairs, roomObjects);
+    //}
 
-    public List<GameObject> GetWeakPairsInScene()
-    {
-        GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("RoomObject");
-        return getPairs(WeakPairs, roomObjects);
-    }
+    //public List<GameObject> GetWeakPairsInScene()
+    //{
+    //    GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("RoomObject");
+    //    return getPairs(WeakPairs, roomObjects);
+    //}
 
-    public List<GameObject> GetPairsInScene()
-    {
-        GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("RoomObject");
-        List<GameObject> pairs = new List<GameObject>();
-        pairs.AddRange(getPairs(StrongPairs, roomObjects));
-        pairs.AddRange(getPairs(WeakPairs, roomObjects));
-        return pairs;
-    }
+    //public List<GameObject> GetPairsInScene()
+    //{
+    //    GameObject[] roomObjects = GameObject.FindGameObjectsWithTag("RoomObject");
+    //    List<GameObject> pairs = new List<GameObject>();
+    //    pairs.AddRange(getPairs(StrongPairs, roomObjects));
+    //    pairs.AddRange(getPairs(WeakPairs, roomObjects));
+    //    return pairs;
+    //}
 
-    private List<GameObject> getPairs(List<GameObject> baseList, GameObject[] roomObjects)
-    {
-        List<GameObject> pairs = new List<GameObject>();
-        foreach (GameObject obj in roomObjects)
-        {
-            if (obj != this.gameObject
-                && baseList.Contains(obj))
-            {
-                //TODO: Sort by strength?
-                pairs.Add(obj);
-            }
-        }
+    //private List<GameObject> getPairs(List<GameObject> baseList, GameObject[] roomObjects)
+    //{
+    //    List<GameObject> pairs = new List<GameObject>();
+    //    foreach (GameObject obj in roomObjects)
+    //    {
+    //        if (obj != this.gameObject
+    //            && baseList.Contains(obj))
+    //        {
+    //            //TODO: Sort by strength?
+    //            pairs.Add(obj);
+    //        }
+    //    }
 
-        return pairs;
-    }
+    //    return pairs;
+    //}
+
 }
--- SPOOPY/Assets/Resources/Scripts/Sorter.cs
+++ SPOOPY/Assets/Resources/Scripts/Sorter.cs
@@ -2,34 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 
 public class Sorter : MonoBehaviour
 {
-    List<GameObject> roomObjects;
-    // Use this for initialization
-    void Start()
-    {
+    //List<GameObject> roomObjects;
+    //// Use this for initialization
+    //void Start()
+    //{
 
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
 
-    }
+    //}
 
-    public void DoPairing()
-    {
-        roomObjects = GameObject.FindGameObjectsWithTag("RoomObject").ToList();
+    //public void DoPairing()
+    //{
+    //    roomObjects = GameObject.FindGameObjectsWithTag("RoomObject").ToList();
 
-        List<GameObject> strongObjects = (List<GameObject>)(
-            from obj in roomObjects 
-            where obj.GetComponent<Pairing>().StrongObject.Equals(true) 
-            select obj);
+    //    List<GameObject> strongObjects = (List<GameObject>)(
+    //        from obj in roomObjects 
+    //        where obj.GetComponent<Pairing>().StrongObject.Equals(true) 
+    //        select obj);
 
-        List<GameObject> weakObjects = (List<GameObject>)(
-            from obj in roomObjects
-            where obj.GetComponent<Pairing>().StrongObject.Equals(false)
-            select obj);
-    }
+    //    List<GameObject> weakObjects = (List<GameObject>)(
+    //        from obj in roomObjects
+    //        where obj.GetComponent<Pairing>().StrongObject.Equals(false)
+    //        select obj);
+
+    //    foreach(GameObject sobj in strongObjects)
+    //    {
+    //        Pairing sobjPairing = sobj.GetComponent<Pairing>();
+    //        GameObject bestPair;
+    //        int bestStrength;
+    //        foreach (GameObject obj in roomObjects)
+    //        {
+    //            Pairing objPairing = obj.GetComponent<Pairing>();
+    //            if (!obj.Equals(this.gameObject)
+    //                && !objPairing.CurrentPairs.Contains(this.gameObject))
+    //            {
+
+    //            }
+    //        }
+    //    }
+    //}
 }
