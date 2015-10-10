548fb65c82f1edd45fab4a0d2429582ace1b946c
Noah Ratcliff <me@nratcliff.com>
2015-10-10 15:32:23 -0400

    Set the room up I think
    Unity 3D scaling is hard

---
+++ SPOOPY/Assets/Resources/Scripts/Paring.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Paring : MonoBehaviour
+{
+    public List<GameObject> StrongPairs; //first round pairings
+    public List<GameObject> WeakPairs; //second round pairings
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
